4a1c88dbcc6f6949535568e5683dd4d8265070ce
Allen <allenlovatt@gmail.com>
2021-03-07 22:19:31 -0500

    Columns Defined - Tag.JS

--- models/Tag.js
+++ models/Tag.js
@@ -5,9 +5,17 @@ const sequelize = require('../config/connection.js');
 class Tag extends Model {}
 
 Tag.init(
-  {
-    // define columns
-  },
+{
+    id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    allowNull: false,
+    autoIncrement: true
+},
+    tag_name: {
+    type: DataTypes.STRING}
+},
+
   {
     sequelize,
     timestamps: false,
